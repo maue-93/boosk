5f47a15a8c218767b923be20dbf6f790c964d595
maue-93 <morazara.c@gmail.com>
2024-11-29 19:01:41 -0500

    setting up Config Module and connect app to mongodb

--- backend/boosk/src/app.module.ts
+++ backend/boosk/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // make ConfigModule available globally and tell the app where the config file is
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+
+    // connect to the mongodb database
+    MongooseModule.forRootAsync({
+      // we need to get the path to the database from our config module
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
